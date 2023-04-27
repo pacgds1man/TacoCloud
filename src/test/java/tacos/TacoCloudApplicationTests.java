d4495621ebe84ab35aa55c6cb5109a0eff39ca7f
ZVER <pacgds1man@gmail.com>
2023-04-27 09:36:55 +0300

    init

---
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -0,0 +1,15 @@
+package tacos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
